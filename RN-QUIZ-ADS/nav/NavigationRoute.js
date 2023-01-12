5cf55a7f8b7cf5f450c536a0837300a4b7a3f00e
Jason Capili David <jaycd598@gmail.com>
2023-01-12 10:30:29 +0800

    new catalog ui

--- RN-QUIZ-ADS/nav/NavigationRoute.js
+++ RN-QUIZ-ADS/nav/NavigationRoute.js
@@ -7,15 +7,12 @@ const Stack = createStackNavigator()
 //Screens
 import Category from '../screens/category/Category';
 import Main from '../screens/Main';
-import HowToPlay from '../screens/HowToPlay';
 
 const NavigationRoute = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='MainScreen'>
         <Stack.Screen name="MainScreen" component={Main} options={{ headerShown:false, gestureEnabled:false }}/>
-        <Stack.Screen name="CategoryScreen" component={Category} options={{ headerShown:false, gestureEnabled:false }}/>
-        <Stack.Screen name="HowToPlayScreen" component={HowToPlay} options={{ headerShown:false, gestureEnabled:false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
--- RN-QUIZ-ADS/screens/Main.js
+++ RN-QUIZ-ADS/screens/Main.js
@@ -1,26 +1,99 @@
-import { StyleSheet, Text, TouchableOpacity, View, StatusBar, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, StatusBar, SafeAreaView, Image, ScrollView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const Main = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar animated={true} barStyle='dark-content' />
+      <StatusBar animated={true} barStyle='light' />
       <View style={styles.subContainer}>
-        <View>
-          <Text>QUIZ</Text>
+
+        <View style={{ margin:25, alignItems:'flex-end', justifyContent:'space-between', flexDirection:'row' }}>
+          <View style={{ flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+            <Image style={{ width:30, height:30 }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/4108/4108233.png'}}/>
+            <Text style={{ color:'#fff', marginLeft:8, fontSize:20}}>100</Text>
+          </View>
+          <TouchableOpacity style={{ justifyContent:'center', alignItems:'center'}}>
+            <Ionicons name="person-circle-outline" size={30} color="#8BD8BD" />
+          </TouchableOpacity>
         </View>
-        <View style={styles.buttonParentContainer}>
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.innerButtonContainer} onPress={()=> navigation.navigate('CategoryScreen')}>
-              <Text style={styles.buttonContainerText}>Play</Text>
-            </TouchableOpacity>
+          
+        <View style={{ margin:25, marginTop:0 }}>
+          <Text style={{ color:'#fff', fontSize:35, fontWeight:'800', marginBottom:5 }} >Quiz Game</Text>
+          <Text style={{ color:'#fff', fontSize:18 }} >Have a try with your favorite category and answer all the questions. Start learning!</Text>
+        </View>
+
+        <ScrollView 
+          showsHorizontalScrollIndicator={false} 
+          showsVerticalScrollIndicator={false}>
+          <View style={styles.categoryContainer}>
+            <View style={{ flexDirection:'row', justifyContent:'space-between' }}>
+              <View>
+                <TouchableOpacity style={{ margin:15, width:24 }}>
+                  <Ionicons name="md-play" size={24} color="#fff"/>
+                </TouchableOpacity>
+                <View  style={{ margin:15, marginTop:0 }}>
+                  <Text style={{ color:'#ffffff90', fontSize:12 }} >Level 5</Text>
+                  <Text style={{ color:'#fff', fontSize:25 }} >Mathematics</Text>
+                </View>
+              </View>
+              <View style={{ margin:15, marginTop:0, justifyContent:'center', alignItems:'center' }}>
+                <Image style={{ width:100, height:100}} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/1375/1375589.png'}}/>
+              </View>
+            </View>
           </View>
 
-          <View style={[styles.buttonContainer, { marginTop:10 }]}>
-            <TouchableOpacity style={styles.innerButtonContainer} onPress={()=> navigation.navigate('HowToPlayScreen')}>
-              <Text style={styles.buttonContainerText}>How to Play</Text>
-            </TouchableOpacity>
+          <View style={styles.categoryContainer}>
+            <View style={{ flexDirection:'row', justifyContent:'space-between' }}>
+              <View>
+                <TouchableOpacity style={{ margin:15, width:24 }}>
+                  <Ionicons name="md-lock-closed" size={24} color="#fff" />
+                </TouchableOpacity>
+                <View  style={{ margin:15, marginTop:0 }}>
+                  <Text style={{ color:'#ffffff90', fontSize:12 }} >Level 1</Text>
+                  <Text style={{ color:'#fff', fontSize:25 }} >Science</Text>
+                </View>
+              </View>
+              <View style={{ margin:15, marginTop:0, justifyContent:'center', alignItems:'center' }}>
+                <Image style={{ width:100, height:100}} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/8152/8152739.png'}}/>
+              </View>
+            </View>
           </View>
-        </View>
+
+          <View style={styles.categoryContainer}>
+            <View style={{ flexDirection:'row', justifyContent:'space-between' }}>
+              <View>
+                <TouchableOpacity style={{ margin:15, width:24 }}>
+                  <Ionicons name="md-lock-closed" size={24} color="#fff" />
+                </TouchableOpacity>
+                <View  style={{ margin:15, marginTop:0 }}>
+                  <Text style={{ color:'#ffffff90', fontSize:12 }} >Level 1</Text>
+                  <Text style={{ color:'#fff', fontSize:25 }}>Technology</Text>
+                </View>
+              </View>
+              <View style={{ margin:15, marginTop:0, justifyContent:'center', alignItems:'center' }}>
+                <Image style={{ width:100, height:100}} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/2712/2712037.png'}}/>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.categoryContainer}>
+            <View style={{ flexDirection:'row', justifyContent:'space-between' }}>
+              <View>
+                <TouchableOpacity style={{ margin:15, width:24 }}>
+                  <Ionicons name="md-lock-closed" size={24} color="#fff" />
+                </TouchableOpacity>
+                <View  style={{ margin:15, marginTop:0 }}>
+                  <Text style={{ color:'#ffffff90', fontSize:12 }} >Level 1</Text>
+                  <Text style={{ color:'#fff', fontSize:25 }}>History</Text>
+                </View>
+              </View>
+              <View style={{ margin:15, marginTop:0, justifyContent:'center', alignItems:'center' }}>
+                <Image style={{ width:100, height:100}} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/1800/1800174.png'}}/>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+
       </View>
     </SafeAreaView>
   )
@@ -31,12 +104,10 @@ export default Main
 const styles = StyleSheet.create({
     container: {
       flex:1,
-      backgroundColor:'#fff'
+      backgroundColor:'#243665'
     },
     subContainer:{
       flex:1,
-      justifyContent: 'center',
-      alignItems:'center'
     },
     buttonParentContainer: {
       position:'absolute',
@@ -44,9 +115,15 @@ const styles = StyleSheet.create({
       left:25,
       right:25
     },
+    iConParentContainer: {
+      position:'absolute',
+      top:0,
+      left:25,
+      right:0
+    },
     buttonContainer: {
       height:55,
-      backgroundColor:'#D6B702',
+      backgroundColor:'#8BD8BD',
       justifyContent: 'center',
       alignItems:'center',
       borderRadius:10
@@ -59,5 +136,12 @@ const styles = StyleSheet.create({
     },
     buttonContainerText: {
       color:'#fff'
+    },
+    categoryContainer: { 
+      margin:25, 
+      marginTop:0, 
+      height:120, 
+      backgroundColor:'#8BD8BD',
+      borderRadius:10
     }
   });
